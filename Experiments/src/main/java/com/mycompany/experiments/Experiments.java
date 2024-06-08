001decb65785edba9b62bfbcc26d4e771c5ecc62
Ishwarya M <141407604+ishwarya611@users.noreply.github.com>
2024-06-08 23:20:12 +0530

    Add files via upload

---
+++ Experiments/src/main/java/com/mycompany/experiments/Experiments.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.experiments;
+
+/**
+ *
+ * @author munz
+ */
+public class Experiments {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
